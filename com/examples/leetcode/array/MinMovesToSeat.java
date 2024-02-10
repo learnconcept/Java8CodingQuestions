45b0bf71fb3ace49cb82afe457dcb08434018187
learnconcept <learnconcept.java@gmail.com>
2024-02-10 14:07:57 +0530

    MinMovesToSeat

---
+++ com/examples/leetcode/array/MinMovesToSeat.java
@@ -0,0 +1,19 @@
+package com.examples.leetcode.array;
+
+import java.util.Arrays;
+
+public class MinMovesToSeat {
+
+    public static void main(String[] args) {
+        int[] seats = {4,1,5,9};
+        int[] students = {1,3,2,6};
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int moves = 0;
+        for(int i=0; i<seats.length; i++){
+            moves += Math.abs(seats[i]-students[i]);
+        }
+        System.out.println(moves);
+    }
+}
