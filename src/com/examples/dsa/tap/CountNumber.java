837343574e051bcd5465b698ec154be205f3a10a
learnconcept <learnconcept.java@gmail.com>
2026-03-05 12:58:42 +0530

    count digits

---
+++ src/com/examples/dsa/tap/CountNumber.java
@@ -0,0 +1,13 @@
+package com.examples.dsa.tap;
+
+public class CountNumber {
+    static void main() {
+        int num = 10010;
+        int count = 0;
+        while(num>0){
+            num = num/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
