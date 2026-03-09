68681cc8d3bbdfe0182c4f493dd65b415b441276
learnconcept <learnconcept.java@gmail.com>
2026-03-09 09:53:23 +0530

    Find LCM

---
+++ src/com/examples/dsa/tap/FindLCM.java
@@ -0,0 +1,42 @@
+package com.examples.dsa.tap;
+
+import java.util.Scanner;
+
+public class FindLCM {
+
+    // Brute force i.e O(a*b - max(a,b))
+    static int findLCM(int a, int b, int max){
+        while(Boolean.TRUE){
+            if(max%a==0 && max%b==0){
+                return max;
+            }
+            max++;
+        }
+        return 0;
+    }
+
+    // LCM(a,b) =( a * b ) /GCD(a,b)
+    static int findLCMLucid(int a, int b){
+        int gcd = 0;
+        while(a!=b){
+            if(a>b){
+                a=a-b;
+            }else{
+                b=b-a;
+            }
+        }
+        return a;
+    }
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        int max = Math.max(a, b);
+        System.out.println(findLCM(a, b, max));
+        int gcd = findLCMLucid(a, b);
+        System.out.println((a*b)/gcd);
+    }
+}
