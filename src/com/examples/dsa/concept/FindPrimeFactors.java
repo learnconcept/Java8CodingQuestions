998f8b8669c9003c7aa7ddd1858a481a332a69a4
learnconcept <learnconcept.java@gmail.com>
2026-04-01 10:13:57 +0530

    Finding Factors

---
+++ src/com/examples/dsa/concept/FindPrimeFactors.java
@@ -0,0 +1,19 @@
+package com.examples.dsa.concept;
+
+import java.util.Scanner;
+
+public class FindPrimeFactors {
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of factors ");
+        int n = sc.nextInt();
+        findPrimeFactors(n);
+    }
+
+    private static void findPrimeFactors(int n) {
+        while(n>2){
+            if(n%2==0){}
+        }
+    }
+}
