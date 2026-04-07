68672d3f96538f1bf2fb29b8294f52b103edf1dc
learnconcept <learnconcept.java@gmail.com>
2026-04-07 09:10:18 +0530

    Recursive_Fact

---
+++ src/com/examples/dsa/concept/RecursiveFactorial.java
@@ -0,0 +1,15 @@
+package com.examples.dsa.concept;
+
+public class RecursiveFactorial {
+    static int factorial(int n)
+    {
+        if(n == 1 || n == 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    static void main() {
+        Integer value = factorial(3);
+        System.out.println(value);
+    }
+}
